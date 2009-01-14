19635d1c502970660a32348998ccefd2f00b0cbf
crab <crabtw@gmail.com>
2009-01-15 02:03:23 +0800

    remove include urls

--- thread-order.user.js
+++ thread-order.user.js
@@ -2,17 +2,17 @@
 // @name           thread-order
 // @namespace      crabtw@gmail.com
 // @description    let threads of Discuz forum order by dateline
-// @include        http://www.acg-guys.net/*
-// @include        http://mwzg.net/*
-// @include        http://www.piring.com/bbs/tcn/*
+// @include        *
 // ==/UserScript==
 
-var links = document.links;
+if(document.title.search('Discuz') != -1) {
+    var links = document.links;
 
-for(var i = 0; i < links.length; ++i) {
-    if(links[i].href.search(/forumdisplay/) != -1 &&
-        links[i].href.search(/orderby/) == -1
-    ) {
-        links[i].href += '&orderby=dateline';
+    for(var i = 0; i < links.length; ++i) {
+        if(links[i].href.search(/forumdisplay/) != -1 &&
+            links[i].href.search(/orderby/) == -1
+        ) {
+            links[i].href += '&orderby=dateline';
+        }
     }
 }

2d82c70be0bb72c0a99964c1fc5954f43753c053
crab <crabtw@gmail.com>
2011-03-29 22:52:16 +0800

    update

---
+++ dinbendon.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name           dinbendon
+// @namespace      crabtw@gmail.com
+// @description    complete dinbendon captcha
+// @include        http://*dinbendon.net/do/*
+// ==/UserScript==
+
+window.addEventListener('load', function() {
+    if(document.getElementById('signInPanel_signInForm') == null) {
+        return;
+    }
+
+    var input = document.getElementsByName('result')[0];
+    var [_, a, b] = input.parentNode.
+                          previousSibling.
+                          previousSibling.
+                          textContent.
+                          match(/(.*)\+(.*)=/);
+    input.value = parseInt(a, 10) + parseInt(b, 10);
+}, false);
